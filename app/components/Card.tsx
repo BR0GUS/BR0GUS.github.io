d50ab37de45e5768cf2dfd3a1ba8ce3ce81e4d93
Zdeněk Sojka <code-stuff.m52iv@aleeas.com>
2024-01-31 21:22:27 +0100

    Completed the right card

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -2,16 +2,27 @@ import React from "react";
 
 interface CardProps {
   header: string;
+  children?: React.ReactNode;
 }
 
-const Card = ({ header }: CardProps) => {
+const Card = ({ header, children }: CardProps) => {
   return (
-    <div className="flex flex-col bg-bg-300 flex-1 h-screen">
-      <div className="m-4">
-        <div className="bg-accent-100 rounded-3xl">
-          <h1 className="text-8xl py-16">{header}</h1>
+    <div className="flex flex-col flex-1 h-screen">
+      <div className="m-4 h-full">
+        <div className="bg-accent-100 rounded-3xl flex justify-center items-center content-center  h-1/6">
+          <h1 className="text-8xl py-16 ">{header}</h1>
         </div>
-        <div>asd</div>
+        <div className="flex justify-center items-center h-5/6 bg-bg-200 rounded-3xl">
+          {children ? (
+            <div className="2xl:text-3xl text-text-100  flex text-center flex-col justify-center">
+              {React.Children.map(children, (child) => (
+                <div className="my-10">{child}</div>
+              ))}
+            </div>
+          ) : (
+            "ZN"
+          )}
+        </div>
       </div>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,31 @@
+/* eslint-disable react/no-unescaped-entities */
 import Card from "./components/Card";
 
 export default function Home() {
   return (
     <div className="flex gap-56 ">
-      <Card header="Zdeněk Sojka"/>
-      <Card header="About Me"/>
+      <Card header="Zdeněk Sojka"></Card>
+      <Card header="About Me">
+        <p>
+          Hello! I'm a high schooler on the cusp of turning 18, with my roots
+          firmly planted in the Czech Republic. Born on April 17, 2005,
+          programming is not just my passion but the essence of my thinking.
+          Python is my weapon of choice – it's simple, yet powerful enough to
+          breathe life into my creative solutions.
+        </p>
+        <p>
+          Yet, there's another side to me – the thrill-seeker who thrives on
+          parkour. Navigating obstacles with grace, I approach each leap and
+          landing with the same focus and precision that I apply to my coding.
+        </p>
+        <p>
+          As I gear up to graduate from 1. IT Gymnázium, my sights are set on an
+          exciting fusion of the future and the far East – I'm Tokyo-bound,
+          aiming to further my education and blossom into a full-time coding
+          freelancer. Here's to crafting code and vaulting over life's physical
+          and virtual challenges! 🌐🏃
+        </p>
+      </Card>
     </div>
   );
 }
